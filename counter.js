25cd62bc660e880c54724aa0d133066aca65abe2
stephen-creator <108318196+stephen-creator@users.noreply.github.com>
2023-04-28 17:57:18 -0400

    2 files modified

--- counter.js
+++ counter.js
@@ -1,1 +1,5 @@
 // TODO: Add counter code here.
+
+const countSpan = document.getElementById('count');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
